b84993bf820c672edd208d4b86face124b550955
hemanth <applicationcomputer257@gmail.com>
2023-06-24 19:38:59 +0530

    intial commit

---
+++ blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPost = () => {
+  return (
+    <div>
+      blog posts
+    </div>
+  )
+}
+
+export default BlogPost;
---
+++ blog/layout.jsx
@@ -0,0 +1,11 @@
+const layout = ({children}) => {
+  return (
+    <div>
+        <hi>
+            hi this is blog
+        </hi>
+        {children}
+    </div>
+  )
+}
+export default layout
